e1d249343b1affead07fbb4dae5041d264ee984d
zarko <zarkogolocorbin@gmail.com>
2022-03-08 13:34:06 +0100

    add new project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+// import images
+import desktopPause from "./images/pattern-divider-desktop.svg";
+import mobilePause from "./images/pattern-divider-mobile.svg";
+import diceIcon from "./images/icon-dice.svg";
+
+// import css file
+import "./App.css";
 
 function App() {
+  const [advice, setAdvice] = useState("");
+
+  function fetchData() {
+    axios.get("https://api.adviceslip.com/advice").then((res) => {
+      const data = res.data;
+      setAdvice(data.slip);
+    });
+  }
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      {advice && (
+        <div className="advice">
+          <p>ADVICE # {advice.id}</p>
+          <h1>{advice.advice}</h1>
+        </div>
+      )}
+      <picture>
+        <source media="(min-width: 768px)" srcSet={desktopPause} />
+        <img src={mobilePause} alt="" />
+      </picture>
+      <button onClick={fetchData}>
+        <img src={diceIcon} alt="dice icon" />
+      </button>
     </div>
   );
 }
